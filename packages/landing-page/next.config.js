7df6a13ba17be30cb7ffea82109627e53992ebdd
Giorgi <gio@etlyn.com>
2023-06-21 21:48:58 -0500

    refactor: adding next js

--- packages/landing-page/next.config.js
+++ packages/landing-page/next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  images: {
+    loader: "akamai",
+    path: "./",
+    unoptimized: true,
+  },
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+};
